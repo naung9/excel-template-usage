53bc1b8e74fb3e6f766edd44ebc2d9569b4f2872
naung9 <naung.nine@gmail.com>
2021-09-21 11:17:25 +0630

    Added Excel JS and its usage example

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const index = require("./index");
 
-const defaultFile = path.join(__dirname, 'iSGM_Template.xlsx');
+const defaultFile = path.join(__dirname, 'Chikyoku WBS.xlsx');
 if (typeof require !== 'undefined' && require.main === module) {
-    fs.readFile(defaultFile, (err, data)=>{
-        fs.writeFile(path.join(__dirname, 'updated.xlsx'), index.populateTemplate(data), (err)=>{
+    fs.readFile(defaultFile, async (err, data)=>{
+        fs.writeFile(path.join(__dirname, 'updated.xlsx'), await index.readAndWriteExcel(data, "buffer"), (err)=>{
             if(err)console.error(err);
         });
     });
--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const xlsx = require("xlsx-template");
+const Excel = require("exceljs");
 
 function populateTemplate(data,templates, returnType = "base64"){
     let xlsxTemplate = new xlsx(data);
     templates.forEach(template => {
         console.log(template);
-        xlsxTemplate.substitute(template.sheetNo, template.data);
+        xlsxTemplate.substitute(template.sheet, template.data);
     });
     return xlsxTemplate.generate({type: returnType});
 }
 
-exports.populateTemplate = populateTemplate;
+async function readAndWriteExcel(data, values, returnType = "base64"){
+    const workbook = new Excel.Workbook();
+    await workbook.xlsx.load(data);
+    values.forEach(value => {
+        const worksheet = workbook.getWorksheet(value.sheet);
+        for(const cellNo in value.cells){
+            const cellObj = worksheet.getCell(cellNo);
+            cellObj.value = value.cells[cellNo];
+        }
+    });
+    const buffer = await workbook.xlsx.writeBuffer();
+    if(returnType === "base64"){
+        return Buffer.from(buffer).toString('base64');
+    }else {
+        return buffer;
+    }
+}
+module.exports = {populateTemplate, readAndWriteExcel};
 window.populateTemplate = populateTemplate;
+window.readAndWriteExcel = readAndWriteExcel;
+window.Excel = Excel;
+window.xlsx = xlsx;
