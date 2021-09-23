8715e982d81bbee754fbeb0cccada5e7f578ce36
Hein Naung Nine <naungnine@isgm2.com>
2021-09-23 10:21:20 +0630

    Changed Function to be able to use config sheet from excel.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 const xlsx = require("xlsx-template");
 const Excel = require("exceljs");
 
@@ -10,25 +13,48 @@ function populateTemplate(data,templates, returnType = "base64"){
     return xlsxTemplate.generate({type: returnType});
 }
 
-async function readAndWriteExcel(data, values, returnType = "base64"){
+function readAndWriteExcelSync(data, values, callBack,returnType = "base64"){
     const workbook = new Excel.Workbook();
-    await workbook.xlsx.load(data);
-    values.forEach(value => {
-        const worksheet = workbook.getWorksheet(value.sheet);
-        for(const cellNo in value.cells){
-            const cellObj = worksheet.getCell(cellNo);
-            cellObj.value = value.cells[cellNo];
+    workbook.xlsx.load(data).then(_ => {
+        if(Array.isArray(values)){
+            values.forEach(value => {
+                const worksheet = workbook.getWorksheet(value.sheet);
+                for(const cellNo in value.cells){
+                    const cellObj = worksheet.getCell(cellNo);
+                    cellObj.value = value.cells[cellNo];
+                }
+            });
+        }else{
+            const worksheet = workbook.getWorksheet(values.configSheet);
+            let currentSheet = "";
+            const sheetData = [];
+            let obj = {};
+            const index = values.startIndex;
+            worksheet.eachRow((row, rowNumber) => {
+                console.log(rowNumber, row.values);
+                const sheetName = row.values[index];
+                if(sheetName !== undefined && sheetData !== null && sheetName !== currentSheet){
+                    console.log(sheetName);
+                    if(currentSheet !== ""){
+                        sheetData.push(obj);
+                    }
+                    currentSheet = sheetName;
+                    obj = {sheet: sheetName, cells: {}};
+                }
+                obj.cells[row.values[index+1]] = values.data[row.values[index+2]];
+            });
+            sheetData.push(obj);
+            console.log(sheetData);
+            readAndWriteExcelSync(data, sheetData, callBack, returnType);
+            return;
         }
+        workbook.xlsx.writeBuffer().then(buffer => {
+            if(returnType === "base64")callBack(Buffer.from(buffer).toString('base64'));
+            else callBack(buffer);
+        });
     });
-    const buffer = await workbook.xlsx.writeBuffer();
-    if(returnType === "base64"){
-        return Buffer.from(buffer).toString('base64');
-    }else {
-        return buffer;
-    }
 }
-module.exports = {populateTemplate, readAndWriteExcel};
 window.populateTemplate = populateTemplate;
-window.readAndWriteExcel = readAndWriteExcel;
+window.readAndWriteExcelSync = readAndWriteExcelSync;
 window.Excel = Excel;
 window.xlsx = xlsx;
